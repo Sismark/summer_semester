6f135c379645c80fa2462a39eb5224c5ea9e4ffa
Sismark <2893834361@qq.com>
2017-07-03 23:45:19 +0800

    snake

--- samples/Snake/ui.c
+++ samples/Snake/ui.c
@@ -6,21 +6,21 @@
 #define CELL_PIXEL			20
 
 // 用来绘图的颜色
-#define COLOR_SNAKE			RGB(193, 205, 205)
-#define COLOR_FOOD			RGB(153, 255, 51)
+#define COLOR_SNAKE			RGB(220,20,60)
+#define COLOR_FOOD			RGB(69,139,116)
 #define COLOR_BOUNDARY		RGB(139, 134, 130)
-#define COLOR_TEXT			RGB(173,216,230)
+#define COLOR_TEXT			RGB(47,79,79)
 
 // 游戏的参数的设置 
-#define INIT_TIMER_ELAPSE	300	// 初始的时钟周期，确定游戏初始速度
-#define	ONE_LEVELS_SCORES	5	// 每升级一次需要的计分
+#define INIT_TIMER_ELAPSE	200	// 初始的时钟周期，确定游戏初始速度
+#define	ONE_LEVELS_SCORES	1	// 每升级一次需要的计分
 #define INIT_SNAKE_LEN		5	// 蛇的长度
-#define SPEEDUP_RATIO		0.8 // 升级以后时间周期（确定游戏速度）提高的比例。
-#define MAX_X		18	// 游戏界面大小
-#define MAX_Y		20	// 游戏界面大小
+#define SPEEDUP_RATIO		0.95 // 升级以后时间周期（确定游戏速度）提高的比例。
+#define MAX_X		35	// 游戏界面大小
+#define MAX_Y		35	// 游戏界面大小
 #define INIT_X		3	// 蛇的初始位置
 #define INIT_Y		3	// 蛇的初始位置
-#define INIT_DIR	SNAKE_LEFT	// 蛇的初始方向
+#define INIT_DIR	SNAKE_RIGHT	// 蛇的初始方向
 
 /********************************************************************************
 * ##########关于Windows数据类型##########
